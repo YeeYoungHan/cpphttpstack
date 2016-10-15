2f51f277d23445b2ee72ab4fa47e954513a6baf4
Yee Young Han <websearch@naver.com>
2016-10-15 11:16:34 +0900

    리눅스 빌드 오류를 수정함

--- HttpStack/HttpClient.h
+++ HttpStack/HttpClient.h
@@ -34,7 +34,7 @@ public:
 	bool DoGet( const char * pszUrl, std::string & strOutputContentType, std::string & strOutputBody );
 	bool DoPost( const char * pszUrl, const char * pszInputContentType, const char * pszInputBody, std::string & strOutputContentType, std::string & strOutputBody );
 	bool DoPost( const char * pszUrl, HTTP_HEADER_LIST * pclsHeaderList, const char * pszInputContentType, const char * pszInputBody, std::string & strOutputContentType, std::string & strOutputBody );
-	bool CHttpClient::DoSoap( const char * pszUrl, const char * pszSoapAction, const char * pszInputBody, std::string & strOutputBody );
+	bool DoSoap( const char * pszUrl, const char * pszSoapAction, const char * pszInputBody, std::string & strOutputBody );
 
 	void SetRecvTimeout( int iRecvTimeout );
 
