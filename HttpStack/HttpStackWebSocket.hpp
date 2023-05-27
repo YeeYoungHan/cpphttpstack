bbff27d5aeb98297c0aadfba1756b18259a27580
Yee Young Han <websearch@naver.com>
2023-05-27 18:49:36 +0900

    WebSocket 클라이언트로 데이터 전송할 때의 MASK 오류를 수정함

--- HttpStack/HttpStackWebSocket.hpp
+++ HttpStack/HttpStackWebSocket.hpp
@@ -31,15 +31,15 @@ bool CHttpStack::SendWebSocketPacket( const char * pszClientIp, int iClientPort,
 
 	if( iDataLen > 65536 )
 	{
-		iPacketLen = 2 + 8 + iDataLen;
+		iPacketLen = 2 + 8 + iDataLen + 4;
 	}
 	else if( iDataLen > 125 )
 	{
-		iPacketLen = 2 + 2 + iDataLen;
+		iPacketLen = 2 + 2 + iDataLen + 4;
 	}
 	else
 	{
-		iPacketLen = 2 + iDataLen;
+		iPacketLen = 2 + iDataLen + 4;
 	}
 
 	char * pszPacket = (char *)malloc( iPacketLen );
@@ -77,6 +77,10 @@ bool CHttpStack::SendWebSocketPacket( const char * pszClientIp, int iClientPort,
 	{
 		pszPacket[1] = iDataLen;
 	}
+
+	pszPacket[1] |= 0x80;
+	memset( pszPacket + iPayLoadPos, 0, 4 );
+	iPayLoadPos += 4;
 
 	memcpy( pszPacket + iPayLoadPos, pszData, iDataLen );
 
